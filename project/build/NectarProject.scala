ac524917e338affac1b53f9049edff99b87fc7ac
Jon Buffington <jon.buffington@juiceanalytics.com>
2011-01-28 09:59:27 -0500

    Add complete JRebel support to both SBT and Maven builds.
    Note: The SBT build is using a forked AppEnginePlugin that I am publishing locally. I plan to create a pull request for the original author.

--- project/build/NectarProject.scala
+++ project/build/NectarProject.scala
@@ -1,9 +1,7 @@
 import sbt._
+import fi.jawsy.sbtplugins.jrebel.JRebelWebPlugin
 
-class NectarProject(info: ProjectInfo) extends DefaultWebProject(info) {
-  // If you're using JRebel for Lift development, uncomment this line.
-  override def scanDirectories = Nil
-
+class NectarProject(info: ProjectInfo) extends AppengineProject(info) with JRebel with JRebelWebPlugin {
   // Add Maven Local repository for SBT to search for (disable if this doesn't suit you)
   val mavenLocal = "Local Maven Repository" at "file://" + Path.userHome + "/.m2/repository"
 }
