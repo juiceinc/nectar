78164270a5332c4c501cf5dc4ad3c235af09861e
Jon Buffington <jon.buffington@juiceanalytics.com>
2011-01-31 11:17:09 -0500

    Revise sbt-app-engine plugin dependency version to match local modifications.

--- project/plugins/NectarPlugins.scala
+++ project/plugins/NectarPlugins.scala
@@ -3,5 +3,5 @@ import sbt._
 class NectarPlugins(info: ProjectInfo) extends PluginDefinition(info) {
   val jawsyMavenReleases = "Jawsy.fi M2 releases" at "http://oss.jawsy.fi/maven2/releases"
   lazy val jrebelPlugin = "fi.jawsy" % "sbt-jrebel-plugin" % "0.2.1"
-  lazy val appenginePlugin    = "net.stbbs.yasushi" % "sbt-appengine-plugin" % "2.2-SNAPSHOT"
+  lazy val appenginePlugin    = "net.stbbs.yasushi" % "sbt-appengine-plugin" % "2.3-SNAPSHOT"
 }
